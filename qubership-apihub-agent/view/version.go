0e8717e437c92084de0b683ebf893a6953bb3a0d
Aleksandr Karpov <aleksandr.v.karpov.qubership@gmail.com>
2025-10-16 14:09:13 +0300

    chore: remove usage of deprecated API

--- qubership-apihub-agent/view/version.go
+++ qubership-apihub-agent/view/version.go
@@ -17,12 +17,15 @@ package view
 import "time"
 
 type PublishedVersionListView struct {
-	Version         string    `json:"version"`
-	Status          string    `json:"status"`
-	Folder          string    `json:"versionFolder"`
-	CreatedAt       time.Time `json:"createdAt"`
-	CreatedBy       string    `json:"createdBy"`
-	PreviousVersion string    `json:"previousVersion"`
+	Version                  string                 `json:"version"`
+	Status                   string                 `json:"status"`
+	CreatedAt                time.Time              `json:"createdAt"`
+	CreatedBy                map[string]interface{} `json:"createdBy"`
+	VersionLabels            []string               `json:"versionLabels"`
+	PreviousVersion          string                 `json:"previousVersion"`
+	PreviousVersionPackageId string                 `json:"previousVersionPackageId,omitempty"`
+	NotLatestRevision        bool                   `json:"notLatestRevision,omitempty"`
+	ApiProcessorVersion      string                 `json:"apiProcessorVersion"`
 }
 
 type PublishedVersionsView struct {
