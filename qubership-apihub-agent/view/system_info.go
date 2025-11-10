4b70e9f2229fc27f351cfdfdb616f9c0507e42a7
Aleksandr Agishev <aleksandr.agishev.qubership@gmail.com>
2025-11-10 12:58:23 +0500

    fix: add validation for CLOUD_NAME, NAMESPACE and AGENT_NAME env values + refactoring (#61)
    * fix: add validation for CLOUD_NAME, NAMESPACE and AGENT_NAME env values + refactoring
    
    * fix: address review comments

--- qubership-apihub-agent/view/system_info.go
+++ qubership-apihub-agent/view/system_info.go
@@ -14,7 +14,19 @@
 
 package view
 
+import "time"
+
 type SystemInfo struct {
-	BackendVersion string `json:"backendVersion"`
-	InsecureProxy  bool   `json:"-"`
+	BackendVersion   string        `json:"backendVersion"`
+	InsecureProxy    bool          `json:"-"`
+	ApihubUrl        string        `json:"-"`
+	AgentUrl         string        `json:"-"`
+	AccessToken      string        `json:"-"`
+	DiscoveryConfig  string        `json:"-"`
+	CloudName        string        `json:"-"`
+	AgentNamespace   string        `json:"-"`
+	ExcludeLabels    []string      `json:"-"`
+	GroupingLabels   []string      `json:"-"`
+	AgentName        string        `json:"-"`
+	DiscoveryTimeout time.Duration `json:"-"`
 }
